034efed41403561362774cd49aef115269fc940e
mqcah <mqcha94@outlook.jp>
2020-07-09 15:51:55 +0900

    いいね機能　コメント機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ before_action :authenticate_user!, except: [:new]
     @book = Book.new
     @book = Book.find(params[:id])
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'home/about'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  
 
+  resources :books do
+	resource :favorites,only: [:create,:destroy]
+	resource :book_comments,only: [:create,:destroy]
+  end
+
+  resources :relationships,only: [:create,:destroy]
 end
